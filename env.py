2ae5c8d1dd907844cd1e4044500e03902c858db1
Wasim Alrawas <wassimrawwas@gmail.com>
2024-09-23 19:44:20 +0000

    initial commit

---
+++ env.py
@@ -0,0 +1,12 @@
+import os
+
+os.environ['SECRET_KEY'] = 'o*e)h49v5h)(p=4l#zwh=+30+-od9c5g&bk^2b)byo1n2g@sy_'
+os.environ['DATABASE_URL'] = 'postgres://nsqalfzv:X4_G2Fj0Sx5I10df4ueGZ2xHYVCH9fej@cornelius.db.elephantsql.com/nsqalfzv'
+os.environ['AWS_STORAGE_BUCKET_NAME'] = 'your-bucket-name'
+os.environ['AWS_ACCESS_KEY_ID'] = 'your-aws-access-key-id'
+os.environ['AWS_SECRET_ACCESS_KEY'] = 'your-aws-secret-access-key'
+os.environ['STRIPE_PUBLIC_KEY'] = 'your-stripe-public-key'
+os.environ['STRIPE_SECRET_KEY'] = 'your-stripe-secret-key'
+os.environ['STRIPE_WH_SECRET'] = 'your-stripe-webhook-secret'
+os.environ['EMAIL_HOST_USER'] = 'your-email'
+os.environ['EMAIL_HOST_PASS'] = 'your-email-password'
